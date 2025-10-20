faf392ef0bbf8d503aa617e6c4e0fd059aa90a3e
katcheee <kchoi1@sva.edu>
2025-10-20 15:36:43 -0400

    week6-hw

---
+++ Weeks 6/p5-custom-pixel-class-hw/sketch.js
@@ -0,0 +1,169 @@
+// Custom Pixel Display using Classes
+// Advanced version with HSB color mapping and image replacement
+
+// ColorPixel class - represents a single pixel with color analysis
+class ColorPixel {
+  constructor(gridX, gridY, pixelSize) {
+    this.gridX = gridX;
+    this.gridY = gridY;
+    this.pixelSize = pixelSize;
+    this.hsbValue = 0;
+    this.brightness = 0;
+
+    // Screen position
+    this.x = 0;
+    this.y = 0;
+  }
+
+  // Update from video capture at this pixel's grid position
+  updateFromCapture(capture, appWidth, appHeight) {
+    // Calculate position in capture
+    let captureX = floor((this.gridX / cols) * capture.width);
+    let captureY = floor((this.gridY / rows) * capture.height);
+
+    // Calculate screen position
+    this.x = (this.gridX / cols) * appWidth;
+    this.y = (this.gridY / rows) * appHeight;
+
+    // Get pixel offset
+    let offset = (captureY * capture.width + captureX) * 4;
+
+    // Get RGB values
+    let r = capture.pixels[offset];
+    let g = capture.pixels[offset + 1];
+    let b = capture.pixels[offset + 2];
+
+    // Calculate brightness and HSB value
+    this.brightness = (r + g + b) / 10;
+    this.hsbValue = map(this.brightness, 0, 250, 0, 500);
+  }
+
+  // Display as colored image based on HSB value
+  displayAsImage() {
+    // Map hue (0-360) to color images
+    // Based on the HSB color wheel
+    if (this.hsbValue >= 0 && this.hsbValue < 4) {
+      image(red, this.x, this.y);
+    } else if (this.hsbValue >= 45 && this.hsbValue < 90) {
+      image(orange, this.x, this.y);
+    } else if (this.hsbValue >= 90 && this.hsbValue < 135) {
+      image(yellow, this.x, this.y);
+    } else if (this.hsbValue >= 135 && this.hsbValue < 180) {
+      image(green, this.x, this.y);
+    } else if (this.hsbValue >= 180 && this.hsbValue < 225) {
+      image(cyan, this.x, this.y);
+    } else if (this.hsbValue >= 225 && this.hsbValue < 270) {
+      image(blue, this.x, this.y);
+    } else if (this.hsbValue >= 270 && this.hsbValue < 315) {
+      image(pink, this.x, this.y);
+    } else {
+      image(purple, this.x, this.y);
+    }
+  }
+
+  // Display as greyscale rectangle
+  displayAsGreyscale() {
+    fill(this.brightness);
+    rect(this.x, this.y, this.pixelSize, this.pixelSize);
+  }
+
+  // Display as rainbow colored rectangle
+  displayAsRainbow() {
+    push();
+    colorMode(HSB);
+    let hsbColor = color(this.hsbValue, 100, 100);
+    fill(hsbColor);
+    rect(this.x, this.y, this.pixelSize, this.pixelSize);
+    pop();
+  }
+}
+
+// Global variables
+let capture;
+let pixelGrid = [];
+let red, green, blue, yellow, purple, orange, pink, cyan;
+
+// Video and app dimensions
+let scaleValue = 20;
+let videoWidth = 1280;
+let videoHeight = 720;
+let appWidth = 1280;
+let appHeight = 720;
+
+// Grid settings
+let cols, rows;
+// let gridSize = 1;
+let gridSizeX = 1;
+let gridSizeY = 4;
+let pixelSize = 80;
+
+function preload() {
+  // Load color images
+  red = loadImage("images/red.png");
+  green = loadImage("images/green.png");
+  blue = loadImage("images/blue.png");
+  yellow = loadImage("images/yellow.png");
+  purple = loadImage("images/purple.png");
+  orange = loadImage("images/orange.png");
+  pink = loadImage("images/pink.png");
+  cyan = loadImage("images/cyan.png");
+}
+
+function setup() {
+  createCanvas(appWidth, appHeight);
+
+  // Setup video capture
+  capture = createCapture(VIDEO);
+  capture.size(videoWidth / scaleValue, videoHeight / scaleValue);
+  capture.hide();
+
+  // Performance optimization
+  pixelDensity(1);
+  rectMode(CENTER);
+  noStroke();
+
+  // Resize all images
+  red.resize(0, pixelSize);
+  green.resize(0, pixelSize);
+  blue.resize(0, pixelSize);
+  yellow.resize(0, pixelSize);
+  purple.resize(0, pixelSize);
+  orange.resize(0, pixelSize);
+  pink.resize(0, pixelSize);
+  cyan.resize(0, pixelSize);
+
+  // Calculate grid dimensions
+  cols = floor(capture.width / gridSizeX);
+  rows = floor(capture.height / gridSizeY);
+
+  // Create grid of ColorPixel objects using nested loops
+  for (let y = 0; y < rows; y++) {
+    pixelGrid[y] = [];
+    for (let x = 0; x < cols; x++) {
+      pixelGrid[y][x] = new ColorPixel(x, y, pixelSize);
+    }
+  }
+}
+
+function draw() {
+  background(255);
+
+  // Flip video horizontally (like Zoom)
+  translate(width, 0);
+  scale(-1, 1);
+
+  // Load pixel data from video
+  capture.loadPixels();
+
+  // Update and display each pixel using nested loops
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      pixelGrid[y][x].updateFromCapture(capture, appWidth, appHeight);
+
+      // Choose display mode:
+      pixelGrid[y][x].displayAsImage();
+      // pixelGrid[y][x].displayAsGreyscale();
+      // pixelGrid[y][x].displayAsRainbow();
+    }
+  }
+}
